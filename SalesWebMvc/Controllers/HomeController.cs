afcde8610a4f05a42c92abb2e9e7f719882c85cb
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-05 16:00:44 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# course";
+            ViewData["developer"] = "Evandro Lucas da Silva";
+            ViewData["email"] = "evandro@gmail.com";
 
             return View();
         }
